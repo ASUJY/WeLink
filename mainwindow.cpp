cd32deef9cb04684e03b5e3c72a2acbe4c116d60
ASUJY <prime_sujy@foxmail.com>
2026-06-08 13:03:35 +0800

    feat:实现主窗口的移动和缩放功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,3 +38,130 @@ void MainWindow::SetWidgetWinTitle() {
         this->showMaximized();
     });
 }
+
+/**
+ * 根据鼠标坐标(x,y)判断鼠标位于窗口的哪个区域
+ */
+Area MainWindow::GetArea(int x, int y) {
+    auto rect = geometry();             // 获取当前窗口的几何信息(位置和尺寸)
+    constexpr int resizemargin = 10;    // 距离窗口边缘10像素内视为边缘区域
+    if (x <= resizemargin) {            // 左侧边缘
+        if (y >= rect.height() - resizemargin) {
+            return Area::BottomLeft;
+        } else if (y <= resizemargin) {
+            return Area::TopLeft;
+        } else {
+            return Area::Left;
+        }
+    } else if (x >= rect.width() - resizemargin) {  // 右侧边缘
+        if (y >= rect.height() - resizemargin) {
+            return Area::BottomRight;
+        } else if (y <= resizemargin) {
+            return Area::TopRight;
+        } else {
+            return Area::Right;
+        }
+    } else {
+        if (y >= rect.height() - resizemargin) {
+            return Area::Bottom;
+        } else if (y <= resizemargin) {
+            return Area::Top;
+        }
+    }
+    return Area::Center;
+}
+
+/**
+ * 根据区域返回对应的光标形状
+ */
+Qt::CursorShape MainWindow::GetCursorForArea(Area area) {
+    switch (area) {
+        case Top: case Bottom:          return Qt::SizeVerCursor;
+        case Left: case Right:          return Qt::SizeHorCursor;
+        case TopLeft: case BottomRight: return Qt::SizeFDiagCursor;
+        case TopRight: case BottomLeft: return Qt::SizeBDiagCursor;
+        case Center:                    return Qt::SizeAllCursor;
+        default:                        return Qt::ArrowCursor;
+    }
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        m_isMouseDown = true;
+        m_pressRect = geometry();                       // 窗口的当前位置和尺寸(左上角坐标和宽高)
+        m_pressPos = event->globalPosition().toPoint(); // 鼠标按下时的全局位置
+        m_posx = mapFromGlobal(m_pressPos).x();         // 将全局坐标转换为相当于当前窗口左上角的局部坐标
+        m_posy = mapFromGlobal(m_pressPos).y();
+        m_area = GetArea(event->pos().x(), event->pos().y());
+        setCursor(GetCursorForArea(m_area));
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event) {
+    if (!m_isMouseDown) return;
+
+    auto currentPos = event->globalPosition().toPoint();
+    auto moveSize = currentPos - m_pressPos;
+    auto currentRect = m_pressRect;
+
+    setCursor(GetCursorForArea(m_area));
+
+    switch (m_area) {
+    case Top:
+        currentRect.setTop(m_pressRect.top() + moveSize.y());
+        break;
+    case Bottom:
+        currentRect.setBottom(m_pressRect.bottom() + moveSize.y());
+        break;
+    case Left:
+        currentRect.setLeft(m_pressRect.left() + moveSize.x());
+        break;
+    case Right:
+        currentRect.setRight(m_pressRect.right() + moveSize.x());
+        break;
+    case TopLeft:
+        currentRect.setTopLeft(m_pressRect.topLeft() + moveSize);
+        break;
+    case TopRight:
+        currentRect.setTopRight(m_pressRect.topRight() + moveSize);
+        break;
+    case BottomLeft:
+        currentRect.setBottomLeft(m_pressRect.bottomLeft() + moveSize);
+        break;
+    case BottomRight:
+        currentRect.setBottomRight(m_pressRect.bottomRight() + moveSize);
+        break;
+    case Center:
+        move(currentPos.x() - m_posx, currentPos.y() - m_posy);
+        return;
+    }
+
+    const int minW = 700;
+    const int minH = 500;
+    bool needAdjust = false;
+    if (currentRect.width() < minW) {
+        needAdjust = true;
+        if (currentRect.left() != m_pressRect.left()) {
+            currentRect.setLeft(currentRect.right() - minW);
+        } else {
+            currentRect.setRight(currentRect.left() + minW);
+        }
+    }
+    if (currentRect.height() < minH) {
+        needAdjust = true;
+        if (currentRect.top() != m_pressRect.top()) {
+            currentRect.setTop(currentRect.bottom() - minH);
+        } else {
+            currentRect.setBottom(currentRect.top() + minH);
+        }
+    }
+    if (needAdjust || currentRect != geometry()) {
+        setGeometry(currentRect);
+    }
+
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event) {
+    m_isMouseDown = false;
+    unsetCursor();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMouseEvent>
+
+enum Area {
+    Top,
+    Left,
+    Right,
+    Bottom,
+    Center,
+    TopLeft,
+    TopRight,
+    BottomLeft,
+    BottomRight
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,10 +30,24 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
 private:
     void SetWidgetWinTitle();
+    Area GetArea(int x, int y);
+    Qt::CursorShape GetCursorForArea(Area area);
 
 private:
     Ui::MainWindow *ui;
+
+    bool m_isMouseDown = false;
+    QPoint m_pressPos;
+    QRect m_pressRect;
+    int m_posx;
+    int m_posy;
+    Area m_area;
 };
 #endif // MAINWINDOW_H

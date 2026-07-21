f09818f9477167730b22ab771a8f7527d5d8f9c2
ASUJY <prime_sujy@foxmail.com>
2026-07-21 12:31:19 +0800

    refactor:优化friend.hpp

--- model/friend.hpp
+++ model/friend.hpp
@@ -5,37 +5,54 @@
 #include <QString>
 #include "user.hpp"
 #include "message.hpp"
-#include "common.h"
+
 
 class Friend : public User
 {
 public:
     explicit Friend() = default;
-    explicit Friend(int64_t wxid, QString name, QString image, bool mute, QString time, int count, QList<Message> messages):
-        User(wxid, name.toStdString(), image.toStdString()), m_mute(mute), m_time(time), m_count(count), m_messages(messages){}
+    explicit Friend(int64_t wxid,
+                    const QString& name,
+                    const QString& image,
+                    bool mute,
+                    const QString& time, int count, const QList<Message>& messages):
+        User(wxid, name.toStdString(), image.toStdString()),
+        m_mute(mute),
+        m_time(time),
+        m_count(count),
+        m_messages(messages),
+        m_state(FriendState::REJECT){}
 
 
     void SetMute(bool mute) { m_mute = mute; }
-    void SetTime(QString time) { m_time = time; }
+    void SetTime(const QString& time) { m_time = time; }
     void SetCount(int count) { m_count = count; }
-    void AddMessage(Message& message) {m_messages.push_back(message);}
+    void AddMessage(const Message& message)  {m_messages.push_back(message);}
+    void AddMessage(Message&& message) noexcept { m_messages.push_back(std::move(message));}
     void SetState(FriendState state) { m_state = state; }
+    void SetMessages(const QList<Message>& messages) { m_messages = messages; }
+
 
     bool GetMute() const { return m_mute; }
-    QString GetTime() const { return m_time; }
+    const QString& GetTime() const { return m_time; }
     int GetCount() const { return m_count; }
     void AddCount(int num) { m_count += num; }
-    QList<Message> GetMessages() const { return m_messages; }
+    const QList<Message>& GetMessages() const { return m_messages; }
     FriendState GetState() const { return m_state; }
 
-    // void SetMessages(QList<Message> messages) { m_messages = messages; }
+    Friend(const Friend&) = delete;
+    Friend& operator=(const Friend&) = delete;
+    Friend(Friend&&) = default;
+    Friend& operator=(Friend&&) = default;
 
 protected:
-    bool m_mute;                // 表示好友是否被静音的状态
+    bool m_mute = false;                // 表示好友是否被静音的状态
+
     QString m_time;             // 好友最后一条消息的时间
-    int m_count;                // 未读消息数量
+    int m_count = 0;                // 未读消息数量
     QList<Message> m_messages;  // 好友的消息列表
-    FriendState m_state;        // 好友状态，是否已经互相添加为好友
+
+    FriendState m_state = FriendState::REJECT;        // 好友状态，是否已经互相添加为好友
 };
 
 #endif // FRIEND_HPP
